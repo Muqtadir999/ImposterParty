8e93d42d5970f44bae59d9416863df1af6462c99
Muqtadir999 <codecraft7895@gmail.com>
2026-09-05 16:32:32 +0000

    Add Android project

---
+++ app/src/main/java/com/imposterparty/app/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.imposterparty.app
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webView = WebView(this).apply {
+            setBackgroundColor(0xFF07111F.toInt())
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            settings.allowFileAccess = true
+            settings.allowContentAccess = true
+            webViewClient = WebViewClient()
+            loadUrl("file:///android_asset/game.html")
+        }
+
+        setContentView(webView)
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (webView.canGoBack()) webView.goBack() else finish()
+            }
+        })
+    }
+}
